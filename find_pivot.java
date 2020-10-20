81167d442b1ea4a0a71393ec2f2993db905238fd
yavar29 <yavarkhan1997@gmail.com>
2020-10-20 21:54:44 +0530

    modified program find_pivot.java

--- find_pivot.java
+++ find_pivot.java
@@ -1,10 +1,19 @@
 class Solution {
     public int pivotIndex(int[] nums) {
-        int sum = 0, leftsum = 0;
-        for (int x: nums) sum += x;
-        for (int i = 0; i < nums.length; ++i) {
-            if (leftsum == sum - leftsum - nums[i]) return i;
-            leftsum += nums[i];
+        int sum=0;
+        for(int i=0; i< nums.length;i++){
+            sum+=nums[i];
+            nums[i]=sum;
+        }
+        
+        int left=0;
+        
+        for(int i=0; i< nums.length; i++){
+            int right= nums[nums.length -1] - nums[i];
+            if(left==right)
+                return i;
+            
+            left = nums[i];
         }
         return -1;
     }
