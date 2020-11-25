920d4d0e655e39b890b53b0e2e5646d1125b45d2
yavar29 <yavarkhan1997@gmail.com>
2020-11-26 00:30:04 +0530

    added program min_moves_to_equal_arr_elmnt.java

---
+++ min_moves_to_equal_arr_elmnt.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int minMoves(int[] nums) {
+        Arrays.sort(nums);
+        int count=0;
+        for(int i=nums.length -1; i>0;i--){
+            count+=nums[i]-nums[0];
+        }
+        return count;
+    }
+}
